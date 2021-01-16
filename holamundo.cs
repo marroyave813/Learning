c40b11261e4806edcbedb9db1a54a9938c1901e5
omarn8911 <omarandres.narvaez@gmail.com>
2021-01-16 09:54:47 -0500

    Create holamundo.cs

---
+++ holamundo.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo !!");
+
+            // Espera a la pulsación de retorno de carro
+            Console.ReadLine();
+        }
+    }
+}
